feb12e6815365f57e9b60b8ec918f78f9093bbe6
ChrisDed <chrisdedeian@gmail.com>
2021-02-27 15:27:31 -0800

    (Client) adding tabset to member-details component

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [],
@@ -12,12 +13,14 @@ import { ToastrModule } from 'ngx-toastr';
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right"
-    })
+    }),
+	TabsModule.forRoot()
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+	TabsModule
   ]
 })
 export class SharedModule { }
