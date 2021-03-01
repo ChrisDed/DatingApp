9bd941937a86bdaf477e286e00b253b50e1effdb
ChrisDed <chrisdedeian@gmail.com>
2021-03-01 10:51:50 -0800

    (Client) adding a photo uploader Using ng2-file-upload to upload images in the photo-editor component

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -36,7 +36,7 @@ namespace API.Controllers
 		}
 
 		// e.g. api/users/wood
-		[HttpGet("{username}")]
+		[HttpGet("{username}", Name = "GetUser")]
 		public async Task<ActionResult<MemberDto>> GetUser(string username)
 		{
 			return await _userRepository.GetMemberAsync(username);
@@ -71,7 +71,10 @@ namespace API.Controllers
 
 			user.Photos.Add(photo);
 
-			if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+			if (await _userRepository.SaveAllAsync())
+			{
+				return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+			}
 
 			return BadRequest("Problem adding photo");
 		}
